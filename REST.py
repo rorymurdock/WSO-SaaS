b28d0869f9bd43e8e4a776e7677c2a52a84e0975
Rory <rmurdock@woolworths.com.au>
2019-07-20 12:00:20 +1000

    Added HTTP support

--- REST.py
+++ REST.py
@@ -8,13 +8,14 @@ class REST():
     """REST API Framework"""
 
     # Add the varibles as self.
-    def __init__(self, url='', timeout=10, retries=5, headers=None, proxy=None, debug=False):
+    def __init__(self, url='', protocol='https', timeout=10, retries=5, headers=None, proxy=None, debug=False):
         # Import Proxy Server settings
         self.proxies = proxy
         self.url = url
         self.debug = debug
         self.timeout = timeout
         self.retries = retries
+        self.protocol = protocol
 
         # Start the session
         self.sessions = requests.Session()
@@ -22,7 +23,7 @@ class REST():
             self.sessions.headers.update(headers)
 
         retries = Retry(total=self.retries, backoff_factor=1, status_forcelist=[502, 503, 504])
-        self.sessions.mount('https://', HTTPAdapter(max_retries=retries))
+        self.sessions.mount('%s://' % protocol, HTTPAdapter(max_retries=retries))
 
         # Debugging
         if self.debug:
@@ -37,32 +38,32 @@ class REST():
     def get(self, path):
         "HTTP GET"
         connection = self.sessions.get(
-            'https://'+self.url+path, proxies=self.proxies, timeout=self.timeout)
+            self.protocol+'://'+self.url+path, proxies=self.proxies, timeout=self.timeout)
         return connection
 
     # HTTP POST, returns HTTP response object
     def post(self, path, payload=None):
         """HTTP POST"""
         connection = self.sessions.post(
-            'https://'+self.url+path, json=payload, proxies=self.proxies, timeout=self.timeout)
+            self.protocol+'://'+self.url+path, json=payload, proxies=self.proxies, timeout=self.timeout)
         return connection
 
     # HTTP PUT, returns HTTP response object
     def put(self, path, payload):
         """HTTP PUT"""
         connection = self.sessions.put(
-            'https://'+self.url+path, data=payload, proxies=self.proxies, timeout=self.timeout)
+            self.protocol+'://'+self.url+path, data=payload, proxies=self.proxies, timeout=self.timeout)
         return connection
 
     # HTTP DELETE, returns HTTP response object
     def delete(self, path):
         """HTTP DELETE"""
         connection = self.sessions.delete(
-            'https://'+self.url+path, proxies=self.proxies, timeout=self.timeout)
+            self.protocol+'://'+self.url+path, proxies=self.proxies, timeout=self.timeout)
         return connection
 
     def response_headers(self, path):
         """Gets response headers"""
         connection = self.sessions.get(
-            'https://'+self.url+path, proxies=self.proxies, timeout=self.timeout)
+            self.protocol+'://'+self.url+path, proxies=self.proxies, timeout=self.timeout)
         return connection.headers
