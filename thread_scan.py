7ebd161a3987830c87aa85103c696e813ba9f6a3
Rory <rmurdock@woolworths.com.au>
2019-07-20 15:14:37 +1000

    Randomised instance order for better threading

--- thread_scan.py
+++ thread_scan.py
@@ -9,20 +9,13 @@ MAX_INSTANCE = 5000
 
 def list_splitter(instances, threads):
     """Take the instance, range it and split into equal threads"""
-    instances = range(instances)
+    instances = list(range(instances))
 
     # Randomise range to avoid some threads closing early with lots of blanks
     random.shuffle(instances)
 
-    avg = len(instances) / float(threads)
-    out = []
-    last = 0.0
-
-    while last < len(instances):
-        out.append(instances[int(last):int(last + avg)])
-        last += avg
-
-    return out
+    for i in range(0, len(instances), threads):
+        yield instances[i:i+threads]
 
 def check_instance(instance_list):
     """Iterate through the list and check the instance"""
@@ -38,7 +31,7 @@ def check_instance(instance_list):
 
 def start():
     """Start the threads"""
-    split = list_splitter(MAX_INSTANCE, MAX_THREADS)
+    split = list(list_splitter(MAX_INSTANCE, MAX_THREADS))
 
     threads = list()
     for index in range(MAX_THREADS):
